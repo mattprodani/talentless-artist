a0db8e7076eaec56f81752926ad96490926e0d9d
mattprodani <mp5908@nyu.edu>
2022-03-04 21:16:07 -0500

    website basic

---
+++ web/shuffler.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    var arr = [];
+    while(arr.length < 12){
+        var r = Math.floor(Math.random() * 380) + 100;
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+    var ancestor = document.getElementById('collage');
+    var descendents = ancestor.getElementsByTagName('img');
+
+    var i, e, d;
+    for (i = 0; i < descendents.length; ++i) {
+        d =  ("" + arr[i]).padStart(3);
+        descendents[i].src = "/libraries/gogh_512_52i/generated/seed0" + d + ".png";
+    }
+}
